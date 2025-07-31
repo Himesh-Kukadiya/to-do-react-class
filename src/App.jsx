35c137dce60ca3fe60d55c2963ff3e636ed063cb
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2025-07-31 12:48:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor() {super()}
+
+  render() {
+    return <h1> Welcome to the class component based react app.</h1>
+  }
+}
