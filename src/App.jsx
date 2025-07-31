2dcc636f94d3ba93722e754cda7502c202f77083
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2025-07-31 12:57:53 +0530

    feat: tailwind configuration using post css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ export default class App extends React.Component {
   constructor() {super()}
 
   render() {
-    return <h1> Welcome to the class component based react app.</h1>
+    return (
+      <h1 className="text-2xl font-bold inline-block underline">
+        Welcome to the class component based react app.
+      </h1>
+    );
   }
 }
